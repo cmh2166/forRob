f6338f424ddd0e4587c5b0c57e3ce833d46af81d
cmh2166 <cmharlow@gmail.com>
2016-07-06 08:53:42 -0400

    prelim commit

---
+++ README.md
@@ -0,0 +1,35 @@
+#Getty Sources (bibo:Document) to LoC Bibliographic Records
+
+##Request
+data/titles.txt.sorted.gz == ULAN titles extracted and sorted by source URI
+
+From eyeballing, they look ... bad. Really bad.
+
+What I want to accomplish is either:
+  a) improve the value to the point where that's > 0
+  b) demonstrate value == 0 and take it out of the LOD representation
+
+Value additions:
+  - link to LoC bibs, other?
+  - Normalize titles and pull out non-Getty identifiers
+  - ...
+
+##Getty bibo:Document information
+
+433752 bibo:Document instances as of 5 July 2016 SPARQL query at vocab.getty.edu/sparql.
+
+###fields:
+rdf:type | all bibo:Document
+rdfs:seeAlso | always a URL of this structure: http://www.getty.edu/vow/AATSource?find=&logic=AND&note=&sourceid=[identifier]
+dcterms:created | date record was created
+dcterms:modified | date record was modified
+skos:changeNote | points to instances of prov:Activity, prov:Create, prov:Modify, http://example.com/base/Create, http://example.com/base/Modify (last two errors?)
+dc:identifier | Getty identifiers only apparently (10 digits)
+dcterms:title | title. All kinds of mess.
+cc:license | All are http://opendatacommons.org/licenses/by/1.0/
+dcterms:license | same as cc:license. Why both?
+void:inDataset | http://vocab.getty.edu/dataset/aat	has 44927, http://vocab.getty.edu/dataset/tgn	has 3567, http://vocab.getty.edu/dataset/ulan	has 62741
+bibo:shortTitle | Many are same as dcterms:title. Only 742 titles differ. Some URLs, could pull out for better data?
+skos:note | variety of notes. Some identifiers - oclc links, ISBN/ISSNs. Unstructured.
+
+##Match title to LoC bibliographic title
---
+++ sources.py
@@ -0,0 +1,99 @@
+from lxml import etree
+import re
+import rdflib
+import requests
+from rdflib.namespace import RDF, DC, SKOS, DCT
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+BIBO = rdflib.Namespace("http://purl.org/ontology/bibo/")
+SRU = "http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&startRecord=1&recordSchema=marcxml"
+issn = "&query=bath.issn="
+isbn = "&query=bath.isbn="
+# really bad matching here
+title = "&query=dc.title="
+ns = {'zs': "http://www.loc.gov/zing/srw/", 'marc': 'http://www.loc.gov/MARC21/slim'}
+numrec_xp = '/zs:searchRetrieveResponse/zs:numberOfRecords'
+recID_xp = '/zs:searchRetrieveResponse/zs:records/zs:record/zs:recordData/marc:record/marc:controlfield[@tag="001"]'
+title_xp = '/zs:searchRetrieveResponse/zs:records/zs:record/zs:recordData/marc:record/marc:controlfield[@tag="001"]'
+oclc_re = re.compile(r"http://www.worldcat.org/oclc/[0-9]{9}", re.IGNORECASE)
+
+def getDocURIs():
+    print("call sparql query")
+    sparql = SPARQLWrapper("http://vocab.getty.edu/sparql")
+    sparql.setQuery("""
+        SELECT DISTINCT ?doc
+        WHERE { ?doc a bibo:Document}
+    """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    print("parsing results")
+    docs = rdflib.Graph()
+    for result in results["results"]["bindings"]:
+        print("parsing: " + result["doc"]["value"])
+        docs.parse(result["doc"]["value"])
+    print("writing to file")
+    docs.serialize("data/docs.nt", format="nt")
+
+
+def matching():
+    matched = {}
+    match = False
+    docs = rdflib.Graph.parse('data/docs.nt', format='nt')
+    for doc in docs.subjects(RDF.type, BIBO.Document):
+        for note in docs.objects(doc, SKOS.note):
+            if 'ISSN' in note and not match:
+                note = note.toPython().replace('ISSN', '')
+                note = note.replace(':', '')
+                note = re.sub(oclc_re, '', note)
+                note = note.strip()
+                results = requests.get(sru + issn + note).text
+                root = etree.fromstring(results)
+                if root.xpath(numrec_xp, namespaces=ns)[0].text != 0:
+                    bibid = root.xpath(recID_xp, namespace=ns)[0].text
+                    matched[doc.toPython()] = "https://lccn.loc.gov/" + bibid
+                    match = True
+            elif 'ISBN' in note and not match:
+                note = note.toPython().replace('ISBN', '')
+                note = note.replace(':', '')
+                note = re.sub(oclc_re, '', note)
+                note = note.strip()
+                results = requests.get(sru + isbn + note).text
+                root = etree.fromstring(results)
+                if root.xpath(numrec_xp, namespaces=ns)[0].text != 0:
+                    bibid = root.xpath(recID_xp, namespace=ns)[0].text
+                    matched[doc.toPython()] = bibid
+                    match = True
+            elif 'http://www.worldcat.org/oclc/' in note and not match:
+                note = note.strip()
+                results = requests.get(note).status_code
+                if results = '200':
+                    matched[doc.toPython()] = note
+                    match = True
+            elif 'OCLC' in note and not match:
+                note = note.toPython().replace('OCLC', '')
+                note = note.replace(':', '')
+                note = re.sub(oclc_re, '', note)
+                note = note.strip()
+                results = requests.get('http://www.worldcat.org/oclc/' + note).status_code
+                if results = '200':
+                    matched[doc.toPython()] = 'http://www.worldcat.org/oclc/' + note
+                    match = True
+        elif not match:
+            for title_str in docs.objects(doc, DCT.title):
+                note = note.strip()
+                results = requests.get(sru + title_str + note).text
+                root = etree.fromstring(results)
+                if root.xpath(numrec_xp, namespaces=ns)[0].text != 0:
+                    bibid = root.xpath(recID_xp, namespace=ns)[0].text
+                    matched[doc.toPython()] = bibid
+                    match = True
+
+
+
+def main():
+    getDocURIs()
+
+
+if __name__ == '__main__':
+    main()
